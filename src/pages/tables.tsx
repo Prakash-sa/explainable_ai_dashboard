8bb276827b871d6d940fa2ef16f79861550c577b
Prakash-sa <sainiprakash525@gmail.com>
2024-05-03 01:49:59 -0500

    solve error

--- src/pages/tables.tsx
+++ src/pages/tables.tsx
@@ -1,13 +1,7 @@
-import { mdiMonitorCellphone, mdiTableBorder, mdiTableOff } from '@mdi/js'
+
 import Head from 'next/head'
 import React, { ReactElement } from 'react'
-import CardBox from '../components/CardBox'
-import CardBoxComponentEmpty from '../components/CardBox/Component/Empty'
 import LayoutAuthenticated from '../layouts/Authenticated'
-import NotificationBar from '../components/NotificationBar'
-import SectionMain from '../components/Section/Main'
-import SectionTitleLineWithButton from '../components/Section/TitleLineWithButton'
-import TableSampleClients from '../components/Table/SampleClients'
 import { getPageTitle } from '../config'
 
 const TablesPage = () => {
@@ -16,29 +10,6 @@ const TablesPage = () => {
       <Head>
         <title>{getPageTitle('Tables')}</title>
       </Head>
-      <SectionMain>
-        <SectionTitleLineWithButton icon={mdiTableBorder} title="Tables" main>
-
-        </SectionTitleLineWithButton>
-
-        <NotificationBar color="info" icon={mdiMonitorCellphone}>
-          <b>Responsive table.</b> Collapses on mobile
-        </NotificationBar>
-
-        <CardBox className="mb-6" hasTable>
-          <TableSampleClients />
-        </CardBox>
-
-        <SectionTitleLineWithButton icon={mdiTableOff} title="Empty variation" />
-
-        <NotificationBar color="danger" icon={mdiTableOff}>
-          <b>Empty card.</b> When there&apos;s nothing to show
-        </NotificationBar>
-
-        <CardBox>
-          <CardBoxComponentEmpty />
-        </CardBox>
-      </SectionMain>
     </>
   )
 }
